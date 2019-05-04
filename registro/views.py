175a00ce8f916f77111c582fd8b23fadf0781942
Tufik Aued B <tauedb@gmail.com>
2019-05-04 13:37:08 -0500

    Se agrego la vista de index

--- registro/views.py
+++ registro/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import RegistroEnergia
 
 # Create your views here.
+class IndexView(ListView):
+    context_object_name = 'registros_energia'
+    queryset = RegistroEnergia.objects.all()
+    print('**********************************************')
+    print(queryset)
+    print('**********************************************')
+    template_name = 'registro/index.html'
